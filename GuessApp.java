759caae07fa72a6cc5ebd733fc7b54b9728050ff
SIDPAWW <siddhipawar.1508@gmail.com>
2026-02-23 16:25:19 +0530

    game result storage

--- GuessApp.java
+++ GuessApp.java
@@ -4,17 +4,23 @@ import java.util.Scanner;
 
 public class GuessApp{
     public static void main(String[] args) throws InvalidGuessException {
+        Scanner scanner =new Scanner(System.in);
         System.out.println("Welcome to the Guessing Game!");
+        System.out.print("enter name:");
+        String playerName = scanner.nextLine();
         GameConfig config = new GameConfig();
         config.showRules();
-        Scanner scanner =new Scanner(System.in);
+       
         int attempts = 0;
+        int hintsUsed = 0;
+        boolean win = false;
         while (attempts < config.getMaxAttempts()){
             System.out.print("Enter your guess: ");
             int guess = ValidationService.validateInput(scanner.nextLine());
             String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
             System.out.println(result);
             if (result.equals("CORRECT!")){
+                win = true;
                 break;
             }
             // Provide a hint after each incorrect guess, up to 2 hints
@@ -27,6 +33,6 @@ public class GuessApp{
         if (attempts >= config.getMaxAttempts()){
             System.out.println("You've run out of attempts. The number was " + config.getTargetNumber());
         }
-
+        StorageService.saveResult(playerName, attempts, win);
     }
 }
